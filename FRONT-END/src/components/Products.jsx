a5a83afa13458b73ce307cf8be1f4cdd669a86c9
Talaini <talainimohammed@gmail.com>
2023-12-05 19:42:03 +0100

    updates

--- FRONT-END/src/components/Products.jsx
+++ FRONT-END/src/components/Products.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Footer from "./Footer";
 import Navigation from "./Navigation";
 import Data from "./Data"
 import "./style.css"
-const productItems = Data.productItems
+//const productItems = Data.productItems
 
 const Productsection = ({ CartItem, addToCart }) => {
   const [count, setCount] = useState(0)
   const increment = () => {
     setCount(count + 1)
   }
+  const url = "https://www.talaini.tech/api/v1/products";
+    const [products, setProducts] = useState([]);
+
+    const fetchInfo = () => {
+      return fetch(url)
+        .then((res) => res.json())
+        .then((d) => setProducts(d))
+    }
+
+    useEffect(() => {
+      fetchInfo();
+    }, []);
+
  return (
     <div>
       <div><Navigation CartItem={CartItem}/> </div>
     <main className="shop background">
       <div className="product-content  grid1">
 
-      {productItems.map((productItems) => {
+      {products.map((productItems) => {
         return (
           <div className='box'>
             <div className='product mtop'>
@@ -29,7 +42,7 @@ const Productsection = ({ CartItem, addToCart }) => {
                 </div>
               </div>
               <div className='product-details'>
-                <h3>{productItems.name}</h3>
+                <h3>{productItems.name_Product}</h3>
                 <div className='rate'>
                   <i className='fa fa-star'></i>
                   <i className='fa fa-star'></i>
@@ -38,7 +51,7 @@ const Productsection = ({ CartItem, addToCart }) => {
                   <i className='fa fa-star'></i>
                 </div>
                 <div className='price'>
-                  <h4>${productItems.price}.00 </h4>
+                  <h4>${productItems.price_product}.00 </h4>
                   {/* step : 3
                    if hami le button ma click garryo bahne
                   */}
--- FRONT-END/src/components/Signup.jsx
+++ FRONT-END/src/components/Signup.jsx
@@ -1,51 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Login.css';
 import profile from "./../assets/a.png";
 import email from "./../assets/email.jpg";
 import pass from "./../assets/pass.png";
 export default function Signup() {
+  const [userData, setUserData] = useState({
+    adresse: '',
+    dateofbirth_User: '',
+    email: '',
+    first_name_User: '',
+    last_name_User: '',
+    password: '',
+    phone: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+
+  const handleSignup = () => {
+    // Add your logic to handle the signup, such as sending the data to an API
+    try {
+      fetch('https://www.talaini.tech/api/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+    //console.log('Signup data:', userData);
+  };
   return (
-    <div><div className="main-m">
+    <div>
+    <div className="main-m">
     <div className="sub-main">
       <div>
         <div className="imgs">
           <div className="container-image">
             <img src={profile} alt="profile" className="profile"/>
-
           </div>
-
-
         </div>
         <div>
-          <h1>Signup</h1>
-          <div>
-            <img src={email} alt="email" className="email"/>
-            <input type="text" placeholder="user name" className="name"/>
-          </div>
-          <div className="second-input">
-            <img src={pass} alt="pass" className="email"/>
-            <input type="password" placeholder="user name" className="name"/>
-          </div>
-          <div className="second-input">
-            <img src={pass} alt="pass" className="email"/>
-            <input type="password" placeholder="user name" className="name"/>
-          </div>
-         <div className="login-button-m">
-         <button>Register</button>
-         </div>
-          
-           <p className="link">
-             <a href="#">Forgot password ?</a> Or<a href="/signup">Sign Up</a>
-           </p>
-          
-
+          <label>First Name:</label>
+          <input type="text" name="first_name_User" value={userData.first_name_User} onChange={handleChange} />
         </div>
-      </div>
-      
+        <div>
+          <label>Last Name:</label>
+          <input type="text" name="last_name_User" value={userData.last_name_User} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={userData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={userData.password} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Phone:</label>
+          <input type="text" name="phone" value={userData.phone} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input type="text" name="adresse" value={userData.adresse} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Date of Birth:</label>
+          <input type="text" name="dateofbirth_User" value={userData.dateofbirth_User} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="button" onClick={handleSignup}>
+            Signup
+          </button>
+        </div>
 
+  </div>
     </div>
-   </div></div>
+   </div>
+   </div>
   )
 }
-
