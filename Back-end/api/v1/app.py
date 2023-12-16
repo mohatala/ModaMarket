3a72fcaf983102baae2fafaec5859203e6ae7774
Talaini <talainimohammed@gmail.com>
2023-12-16 13:25:22 +0100

    Update app.py

--- Back-end/api/v1/app.py
+++ Back-end/api/v1/app.py
@@ -3,10 +3,8 @@
 import os
 from flask import Flask
 from api.v1.views import app_views
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
